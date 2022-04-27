61731c70453a4164d1d78182ffa88e114c212c3f
Andrew Meredith <andrew.meredith@cudoventures.com>
2022-04-27 02:10:05 +0100

    First draft

---
+++ SPECS/cudos-noded.spec
@@ -0,0 +1,121 @@
+#
+# Copyright 2022 Andrew Meredith <andrew.meredith@cudoventures.com>
+# Copyright 2022 Cudo Ventures - All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Runs the Cudos Node Daemon
+#
+
+Name:         cudos-noded
+Version:      %{_versiontag}
+Release:      %{_releasetag}%{?dist}
+Summary:      Cudos Full Node
+
+License:      GPL3
+URL:          https://github.com/CudoVentures/cudos-node           
+
+Source0:      cudos-noded-%{version}.tar.gz
+Source1:      cudos-noded.service
+Source2:      etc_default_cudos-noded
+Source3:      etc_profiled_cudos-noded.sh
+
+Provides:     libwasmvm.so()(64bit)
+
+BuildRequires: golang
+
+# undefine __brp_mangle_shebangs
+
+%description
+Cudos node binary and library
+%pre
+getent group cudos >/dev/null || groupadd -r cudos || :
+getent passwd cudos >/dev/null || useradd -c "Cudos User" -g cudos -s /bin/bash -r -m -d /var/lib/cudos cudos 2> /dev/null || :
+
+%package -n cudos-node-src
+Summary: CUDOS Node Sources
+Requires: cudos-noded
+%description -n cudos-node-src
+CUDOS Node Sources
+
+%prep
+echo -e "\n\n=== prep section ===\n\n"
+# Unpack tarball
+
+BASEDR="$( pwd )"
+tar xzvf %{SOURCE0}
+
+%build
+echo -e "\n\n=== build section ===\n\n"
+
+cd CudosNode
+
+export BUILD_FLAGS="-X github.com/cosmos/cosmos-sdk/version.Name=cudos-node -X github.com/cosmos/cosmos-sdk/version.ServerName=cudos-noded -X github.com/cosmos/cosmos-sdk/version.Version=%{version} -X github.com/cosmos/cosmos-sdk/version.Commit=%{release}"
+export GOPATH="${RPM_BUILD_DIR}/go"
+
+echo -e "\n\n=== Build and install cudos-noded ===\n\n"
+
+go install -v -mod=readonly -ldflags "${BUILD_FLAGS}" ./cmd/cudos-noded
+
+%install
+echo -e "\n\n=== install section ===\n\n"
+
+# Make the fixed directory structure
+mkdir -p ${RPM_BUILD_ROOT}/var/lib/cudos/cudos-data/config
+mkdir -p ${RPM_BUILD_ROOT}/etc/default/
+mkdir -p ${RPM_BUILD_ROOT}/etc/profile.d/
+mkdir -p ${RPM_BUILD_ROOT}/usr/bin/
+mkdir -p ${RPM_BUILD_ROOT}/usr/lib/systemd/system
+mkdir -p ${RPM_BUILD_ROOT}/lib64
+
+# Copy the sources to /var/lib/cudos
+cp -rv ${RPM_BUILD_DIR}/Cudos*                         ${RPM_BUILD_ROOT}/var/lib/cudos/
+
+# Copy the newly built binaries into /usr/bin and /lib64
+cp -v ${RPM_BUILD_DIR}/go/bin/cudos-noded              ${RPM_BUILD_ROOT}/usr/bin/cudos-noded
+cp -v ${RPM_BUILD_DIR}'/go/pkg/mod/github.com/!cosm!wasm/wasmvm@v0.16.0/api/libwasmvm.so' ${RPM_BUILD_ROOT}/lib64/
+chmod 644 ${RPM_BUILD_ROOT}/lib64/*.so
+
+# Install environment setup files
+cp ${RPM_SOURCE_DIR}/etc_default_cudos-noded           ${RPM_BUILD_ROOT}/etc/default/cudos-noded
+cp ${RPM_SOURCE_DIR}/etc_profiled_cudos-noded.sh       ${RPM_BUILD_ROOT}/etc/profile.d/cudos-noded.sh
+
+# Install systemd service file
+cp ${RPM_SOURCE_DIR}/*.service                         ${RPM_BUILD_ROOT}/usr/lib/systemd/system/
+
+%clean
+# rm -rf $RPM_BUILD_ROOT
+
+%post
+if [ $1 = "1" ]
+then
+    echo "Install .. but no scripts today"
+else
+    echo "Upgrade .. still no scripts today"
+fi
+
+%files
+%defattr(-,root,root,-)
+/etc/default/*
+/etc/profile.d/*
+/usr/bin/cudos-noded
+/usr/lib/systemd/system/cudos-noded.service
+/lib64/*
+%doc
+
+%files -n cudos-node-src
+%defattr(-,cudos,cudos,-)
+/var/lib/cudos/Cudos*
+
+%changelog
---
+++ build.sh
@@ -0,0 +1,142 @@
+#!/bin/bash -i
+#
+# Copyright 2022 Andrew Meredith <andrew.meredith@cudoventures.com>
+# Copyright 2022 Cudo Ventures - All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+#
+# The cudos_version variable needs to be set in the environment
+#
+# It is used to
+# - select the github tags to set when cloning the software
+# - tag the tarball
+# - set in the rpmbuild that creates the binaries
+# - Set as the "version" response in "cudos-noded"
+#
+
+if [ "$cudos_version" = "" ]
+then
+    echo "Error: 'cudos_version' variable unset"
+    exit 1
+fi
+
+#
+# As the correlation between the overall version tag and the tags within the repository
+# has varied over time, and to include the development branches in the build mechanism
+# this CASE is used to fetch the correct githun tags for the version indicated by the
+# "$cudos_version" environment variable.
+#
+# It is intended for use as a step in a CI/CD chain to solidify the sources into a tarball in
+# the conventional location in the rpmbuild tree structure ie ./SOURCES/
+#
+# NB All version control files are removed
+#
+
+case $cudos_version in
+
+0\.4)
+  git clone --depth 1 --branch v0.4.0 https://github.com/CudoVentures/cudos-node.git CudosNode
+  git clone --depth 1 --branch v0.3.3 https://github.com/CudoVentures/cudos-builders.git CudosBuilders
+  git clone --depth 1 --branch v0.4.0 https://github.com/CudoVentures/cosmos-gravity-bridge.git CudosGravityBridge
+  git clone                           https://github.com/CudoVentures/cudos-network-upgrade.git CudosNetworkUpgrade
+  ;;
+
+0\.[5-9]\.[0-9])
+  git clone --depth 1 --branch v$cudos_version https://github.com/CudoVentures/cudos-node.git CudosNode
+  git clone --depth 1 --branch v$cudos_version https://github.com/CudoVentures/cudos-builders.git CudosBuilders
+  git clone --depth 1 --branch v$cudos_version https://github.com/CudoVentures/cosmos-gravity-bridge.git CudosGravityBridge
+  ;;
+  
+[1-9]\.[0-9]\.[0-9])
+  git clone --depth 1 --branch v$cudos_version https://github.com/CudoVentures/cudos-node.git CudosNode
+  git clone --depth 1 --branch v$cudos_version https://github.com/CudoVentures/cudos-builders.git CudosBuilders
+  git clone --depth 1 --branch v$cudos_version https://github.com/CudoVentures/cosmos-gravity-bridge.git CudosGravityBridge
+  ;;
+  
+1.0.master)
+  git clone --depth 1 --branch cudos-master https://github.com/CudoVentures/cudos-node.git CudosNode
+  git clone --depth 1 --branch cudos-master https://github.com/CudoVentures/cudos-builders.git CudosBuilders
+  git clone --depth 1 --branch cudos-master https://github.com/CudoVentures/cosmos-gravity-bridge.git CudosGravityBridge
+  ;;
+  
+1.0.dev)
+  git clone --depth 1 --branch cudos-dev https://github.com/CudoVentures/cudos-node.git CudosNode
+  git clone --depth 1 --branch cudos-dev https://github.com/CudoVentures/cudos-builders.git CudosBuilders
+  git clone --depth 1 --branch cudos-dev https://github.com/CudoVentures/cosmos-gravity-bridge.git CudosGravityBridge
+  ;;
+  
+*)
+  echo "Unknown Version '$cudos_version'"
+  exit 1
+  ;;
+  
+esac
+
+mkdir -p SOURCES
+
+tar czvf SOURCES/cudos-noded-${cudos_version}.tar.gz --exclude-vcs Cudos*
+rm -rf Cudos*
+
+#
+# Clear out the old RPM binary files and the old BUILDROOT
+#
+rm -rv RPMS BUILDROOT
+
+#
+# BUILD_NUMBER can be inherited from the CI/CD environment and
+# represent the serial number of that build for traceability
+#
+# If unset, tag it with the hostname and datestamp
+#
+# This is used as
+# - The "release_tag" in the rpm packaging
+# - The minor release embeded in the cudos-noded binary
+#
+if [ "$BUILD_NUMBER" = "" ]
+then
+	BUILD_NUMBER="$( hostname -s ).$( date '+%Y%m%d%H%M%S' )"
+fi
+
+#
+# Pass control to rpmbuild and the rpm spec file
+#
+rpmbuild --define "_topdir $( pwd )" --define "_versiontag ${cudos_version}" --define "_releasetag ${BUILD_NUMBER}" -ba $( pwd )/SPECS/cudos-noded.spec
+
+#
+# Feed the rpm binaries into "Alien" to be converted
+# to Debian packages
+#
+mkdir -p debian
+cd debian
+for FNM in ../RPMS/*/*.rpm
+do
+   echo -e "\n\nConverting rpm file $FNM to deb package\n\n"
+   sudo alien --to-deb --keep-version --scripts $FNM
+done
+cd ..
+
+#
+# Pull the rpm backage information into .txt files
+# Pull the file list form the packages into .lst.txt files
+#
+# These are usesful to store alongside the packages themselves in artifact lists
+# So the content and info can easily be opened in the artifact list and viewed
+#
+for RPMFILE in RPMS/*/*.rpm
+do
+	rpm -qip $RPMFILE > ${RPMFILE}.txt
+	rpm -qlp $RPMFILE > ${RPMFILE}-lst.txt
+done

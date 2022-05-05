382941e6ae2209b1ca0083f525a71bb4e944cd5e
Andrew Meredith <andrew.meredith@cudoventures.com>
2022-05-06 00:54:43 +0100

    Update

--- SOURCES/cudos-noded-ctl.sh
+++ SOURCES/cudos-noded-ctl.sh
@@ -17,7 +17,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # The CUDOS_HOME variable needs to be set in order to fix the location
-# of the config files and database
+# of the config files and database.
 #
 
 #
